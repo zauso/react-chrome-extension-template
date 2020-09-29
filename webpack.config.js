5f4b01156832cda630dd1be708b44bb65caa4009
Serhii <ser.yeremenko@gmail.com>
2020-09-29 20:18:04 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+         test: /\.(json)$/,
+         use: [
+           'file-loader',
+         ],
+       }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new CopyWebpackPlugin({
+		patterns: [{
+			from: './src/config/manifest.json',
+			to: path.join(__dirname, 'dist'),
+			force: true,
+		}]
+	})
+  ]
+};
+
